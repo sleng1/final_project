d9f1fbb7e0283c735c1fb2ed0606b765b27068f4
s-leng <leng.seb@gmail.com>
2024-05-30 00:05:34 -0700

    c

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,16 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+import adapter from '@sveltejs/adapter-static';
+import {
+    vitePreprocess
+} from '@sveltejs/kit/vite';
+ 
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    preprocess: vitePreprocess(),
+ 
+    kit: {
+        adapter: adapter()
+    }
+};
+ 
+export default config;
